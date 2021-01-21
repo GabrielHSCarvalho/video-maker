ac7d423d2c77320634d1257ecb6d43105d786dc4
Gabriel Carvalho <ghccabral@gmail.com>
2021-01-21 12:44:02 -0300

    feat: download das imagens

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state.js')
@@ -7,9 +8,10 @@ const googleSearchCredentias = require('../credentials/google-search.json')
 async function robot() {
 	const content = state.load()
 	
-	await fetchImagesOfAllSenteces(content)
+	//await fetchImagesOfAllSenteces(content)
+	await downloadAllImages(content)
 	
-	state.save(content)
+	//state.save(content)
 	
 	async function fetchImagesOfAllSenteces(content) {
 		for (const sentence of content.sentences) {
@@ -29,11 +31,44 @@ async function robot() {
 			num: 2
 		})
 		
-		const imagesUrl = response.data.items.map((item) => {
+		const imageUrl = response.data.items.map((item) => {
 			return item.link
 		})
-		return imagesUrl
+		return imageUrl
 	}
+	
+	async function downloadAllImages(content) {
+		content.downloadedImages = []
+
+		for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+			const images = content.sentences[sentenceIndex].images
+
+			for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+				const imageUrl = images[imageIndex]
+
+				try {
+					if (content.downloadedImages.includes(imageUrl)) {
+						throw new Error('Image already downloaded')
+					}
+
+					await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+					content.downloadedImages.push(imageUrl)
+					console.log(`> [image-robot] [${sentenceIndex}][${imageIndex}] Image successfully downloaded: ${imageUrl}`)
+					break
+				} catch(error) {
+					console.log(`> [image-robot] [${sentenceIndex}][${imageIndex}] Error (${imageUrl}): ${error}`)
+				}
+			}
+		}
+	}
+
+	async function downloadAndSave(url, fileName) {
+		return imageDownloader.image({
+			url: url,
+			dest: `./content/${fileName}`
+		})
+	}
+	
 	
 }
 
